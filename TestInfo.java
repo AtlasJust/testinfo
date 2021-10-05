d70b8ce5b13d6dd27dbadeff2fa01b2bcc025102
Jesse Just <87329429+AtlasJust@users.noreply.github.com>
2021-10-05 09:03:45 -0400

    Add files via upload

---
+++ TestInfo.java
@@ -0,0 +1,7 @@
+//chapter 3 demo excersise
+public class TestInfo {
+    public static void main(String[] args){
+        System.out.println("calling display info from another class");
+        ParadiseInfo.displayInfo();
+    }
+}
